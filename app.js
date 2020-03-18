dd07e0c036a9f6e715c80f0ddad57e065d784571
IshitaB08 <f20180306@pilani.bits-pilani.ac.in>
2020-03-18 21:47:45 +0530

    RESTful Routing

--- app.js
+++ app.js
@@ -1,39 +1,112 @@
-var express = require('express');
-var app = express();
-var bodyParser = require("body-parser");
-
-var campgrounds = [
-    {name: "Santorini Blues", image: "https://images.unsplash.com/photo-1530841377377-3ff06c0ca713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-    {name: "Nevada Creek", image: "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"},
-    {name: "Montana Hills", image: "https://images.unsplash.com/photo-1507163525711-618d70c7a8f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1048&q=80"}
-];
+var express = require('express'),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+// var campgrounds = [
+//     {name: "Santorini Blues", image: "https://images.unsplash.com/photo-1530841377377-3ff06c0ca713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+//     {name: "Nevada Creek", image: "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"},
+//     {name: "Montana Hills", image: "https://images.unsplash.com/photo-1507163525711-618d70c7a8f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1048&q=80"},
+//     {name: "Santorini Blues", image: "https://images.unsplash.com/photo-1530841377377-3ff06c0ca713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+//     {name: "Nevada Creek", image: "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"},
+//     {name: "Montana Hills", image: "https://images.unsplash.com/photo-1507163525711-618d70c7a8f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1048&q=80"}
+// ];
+
+mongoose.connect("mongodb://localhost/yelpcamp");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//         name: "Nevada Creek",
+//         image: "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
+//         description: "This is serene, no bathrooms, beautiful hills and resting rooms."
+//     },
+//     function (err, campground) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("NEWLY CREATED:");
+//             console.log(campground);
+//         }
+//     }
+// );
+
+app.get("/", function (req, res) {
     res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+
+//INDEX
+app.get("/campgrounds", function (req, res) {
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {
+                campgrounds: allCampgrounds
+            });
+        }
+    });
 });
 
-app.post("/campgrounds", function(req, res){
+
+//CREATE
+app.post("/campgrounds", function (req, res) {
     // res.send('post route');
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    var desc = req.body.description;
+    var newCampground = {
+        name: name,
+        image: image,
+        description: desc
+    };
+    // campgrounds.push(newCampground);
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        };
+    });
 });
 
-app.get("/campgrounds/new", function(req, res){
+
+//NEW
+app.get("/campgrounds/new", function (req, res) {
     res.render("new.ejs");
 });
 
-app.listen(3000, function(){
-    console.log('yelpcamp');
+
+//SHOW
+app.get("/campgrounds/:id", function (req, res) {
+    // Find campground with provided id
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {
+                campground: foundCampground
+            });
+        }
+    });
+
+    // Render show template with that campground
+
 });
 
+app.listen(3000, function () {
+    console.log('yelpcamp');
+});
